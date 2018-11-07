a10c4ed3daa80994feaa26ceac5a790e3b758f54
vahagn85 <v.ulikhanyan@yandex.ru>
2018-11-07 21:06:57 +0400

    Updates

---
+++ precache-manifest.bbb866921e92d5866d51bbf5e5790759.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/potfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "55ea43d5b63c5d1d8027",
+    "url": "/potfolio/static/js/runtime~main.55ea43d5.js"
+  },
+  {
+    "revision": "9e53e0bece4b8b2c830b",
+    "url": "/potfolio/static/js/main.9e53e0be.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/potfolio/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "9e53e0bece4b8b2c830b",
+    "url": "/potfolio/static/css/main.6fd0a3ca.chunk.css"
+  },
+  {
+    "revision": "1effc016032b51b43583898aaa77b355",
+    "url": "/potfolio/index.html"
+  }
+];
