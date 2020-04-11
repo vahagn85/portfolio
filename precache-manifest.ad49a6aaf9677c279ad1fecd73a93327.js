f384d13f89c349f746140a04d933fcbca0173496
vahagn85 <v.ulikhanyan@yandex.ru>
2020-04-11 17:14:01 +0400

    Updates

---
+++ precache-manifest.ad49a6aaf9677c279ad1fecd73a93327.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7e2854dfcedf02e21c78e34bd7a7141",
+    "url": "/potfolio/static/media/SFProText-Regular.c7e2854d.woff2"
+  },
+  {
+    "revision": "cf96b254e0e3f9b792e0",
+    "url": "/potfolio/static/css/main.281ca42b.chunk.css"
+  },
+  {
+    "revision": "c1105e8cedae156074e7",
+    "url": "/potfolio/static/js/1.c1105e8c.chunk.js"
+  },
+  {
+    "revision": "55ea43d5b63c5d1d8027",
+    "url": "/potfolio/static/js/runtime~main.55ea43d5.js"
+  },
+  {
+    "revision": "939ecb2df9e816b5d7e567b851f67121",
+    "url": "/potfolio/static/media/bg.939ecb2d.jpg"
+  },
+  {
+    "revision": "fe04134d1236479bcddd20f6bd5e8e0d",
+    "url": "/potfolio/static/media/vahagn-photo.fe04134d.jpg"
+  },
+  {
+    "revision": "74276d96c676de77ace354436456f651",
+    "url": "/potfolio/static/media/vahagn-am.74276d96.pdf"
+  },
+  {
+    "revision": "e8f7a2aea49761d9945e5ebed4278aad",
+    "url": "/potfolio/static/media/vahagn-en.e8f7a2ae.pdf"
+  },
+  {
+    "revision": "cf96b254e0e3f9b792e0",
+    "url": "/potfolio/static/js/main.cf96b254.chunk.js"
+  },
+  {
+    "revision": "06b45761bfabccf92c66efc6e3d6fbec",
+    "url": "/potfolio/static/media/SFProText-Regular.06b45761.woff"
+  },
+  {
+    "revision": "e1cf4650f7cbd4b7c94117dd07478082",
+    "url": "/potfolio/static/media/SFProText-Regular.e1cf4650.ttf"
+  },
+  {
+    "revision": "53adde59ed16e5b80d3dcc1a6a71c2cd",
+    "url": "/potfolio/static/media/SFProText-Regular.53adde59.eot"
+  },
+  {
+    "revision": "211b9d5260f9bb835f5635c688c92e6e",
+    "url": "/potfolio/static/media/SFProText-Semibold.211b9d52.woff2"
+  },
+  {
+    "revision": "f9ef29896b27001f1dbaa00acbd5408c",
+    "url": "/potfolio/static/media/SFProText-Semibold.f9ef2989.woff"
+  },
+  {
+    "revision": "a8e84b27c0b2865df1e8c3de0ade9c1d",
+    "url": "/potfolio/static/media/SFProText-Semibold.a8e84b27.eot"
+  },
+  {
+    "revision": "f1e257769255afb5fd40f83249826660",
+    "url": "/potfolio/static/media/SFProText-Semibold.f1e25776.ttf"
+  },
+  {
+    "revision": "e0353c7fafe5c6eb3be3e55e36f50eb3",
+    "url": "/potfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.30b21aa87e091945f8293b40f7dc2888.js"
+  "/potfolio/precache-manifest.ad49a6aaf9677c279ad1fecd73a93327.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/potfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
