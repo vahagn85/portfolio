0338222b66e1055bfbe7251a48676aaf9c7a41c3
vahagn85 <v.ulikhanyan@yandex.ru>
2025-08-08 19:11:42 +0400

    refactor: update data

--- src/components/education/index.js
+++ src/components/education/index.js
@@ -1,43 +1,50 @@
-import React from 'react';
-import {LanguageConsumer, TranslatableText} from "../../home";
-const Education =(props)=> {
-    const renderEdu =()=> {
-        return (
-            <div className="edu_work_wrapper edu_wrap">
-                <LanguageConsumer>
-                    {
-                        ({language}) => {
-                            const lang = language === 'english' ? 'en' : 'am';
-                            return (
-                                props.data[lang].edu.map(item => {
-                                    return (
-                                        <div className="edu_work_single" key={item.id}>
-                                            <span className="edu_work_title txt-cl-green">{item.title}</span>
-                                            <p className="edu_work_info">{item.info}</p>
-                                        </div>
-                                    )
-                                })
-                            )
-                        }
-                    }
-                </LanguageConsumer>
-            </div>
-        )
-    }
-        return (
-            <div className="edu_wrapper">
-                <h2>
-                    <TranslatableText
-                        dictionary={{
-                            english: props.data.en.title,
-                            armenian: props.data.am.title,
-                        }}
-                    />
-                </h2>
-                {renderEdu()}
-            </div>
-        );
-
-}
+import React from "react";
+import { LanguageConsumer, TranslatableText } from "../../home";
+const Education = (props) => {
+  const renderEdu = () => {
+    return (
+      <div className="edu_work_wrapper edu_wrap">
+        <LanguageConsumer>
+          {({ language }) => {
+            const lang = language === "english" ? "en" : "am";
+            return props.data[lang].edu.map((item) => {
+              return (
+                <div className="edu_work_single" key={item.id}>
+                  <span className="edu_work_title txt-cl-green">
+                    {item.title}
+                  </span>
+                  <p className="edu_work_info">{item.info}</p>
+                  {item.certificate && (
+                    <a
+                      href={item.certificate}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text_info"
+                    >
+                      Certificate
+                    </a>
+                  )}
+                </div>
+              );
+            });
+          }}
+        </LanguageConsumer>
+      </div>
+    );
+  };
+  return (
+    <div className="edu_wrapper">
+      <h2>
+        <TranslatableText
+          dictionary={{
+            english: props.data.en.title,
+            armenian: props.data.am.title,
+          }}
+        />
+      </h2>
+      {renderEdu()}
+    </div>
+  );
+};
 
 export default Education;
